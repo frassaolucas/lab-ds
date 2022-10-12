41b370506f9e02f399e1ed85f1fbc55833a4f699
frassaolucas <frassao.lucas@gmail.com>
2022-10-12 15:35:16 -0300

    created checkbox component and storybook

---
+++ src/components/checkbok/Checkbox.tsx
@@ -0,0 +1,14 @@
+import * as RadixCheckbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
+
+export type CheckboxProps = {};
+
+export const Checkbox: React.FC<CheckboxProps> = () => {
+  return (
+    <RadixCheckbox.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+      <RadixCheckbox.Indicator asChild>
+        <Check weight="bold" className="w-5 h-5 text-cyan-500" />
+      </RadixCheckbox.Indicator>
+    </RadixCheckbox.Root>
+  );
+};
