e518241bea68d77c265326346cc35922feef3e58
frassaolucas <frassao.lucas@gmail.com>
2022-10-11 22:46:10 -0300

    created storybook text component

---
+++ src/components/text/Text.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      defaultValue: 'md',
+      control: {
+        type: 'inline-radio',
+        labels: {
+          sm: 'Small',
+          md: 'Medium',
+          lg: 'Large',
+        },
+      },
+    },
+  },
+  args: {
+    children: 'Lorem ipsum',
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: <p>Paragraph example</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
---
+++ src/components/text/Text.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+
+export type TextProps = {
+  size?: 'sm' | 'md' | 'lg';
+  asChild?: boolean;
+  children: ReactNode;
+};
+
+export const Text: React.FC<TextProps> = ({ size, asChild, children }) => {
+  const Comp = asChild ? Slot : 'span';
+
+  return (
+    <Comp
+      className={clsx('text-gray-100 font-sans', {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg',
+      })}
+    >
+      {children}
+    </Comp>
+  );
+};
+
+Text.defaultProps = {
+  size: 'md',
+  asChild: false,
+};
